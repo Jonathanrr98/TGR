49bf2dc155e3e963a8eaae586856dbd1515ebf10
Jonathan Ramírez <jonathanrruci@gmail.com>
2023-03-29 09:00:49 -0500

    Login actualizado con logo y sin el underline de los inputs

--- TGR-front_end/src/Routes/LoginRestreador.jsx
+++ TGR-front_end/src/Routes/LoginRestreador.jsx
@@ -2,79 +2,65 @@ import * as React from "react";
 import Layout from "../Components/Layout";
 import { FormGroup, Typography } from "@mui/material";
 import { Box, Button, Input } from "@mui/material";
+import Logo from "../assets/Logo.png";
 
 export const LoginRestreador = () => {
   return (
-    <Layout>
-      <Box
+    <Box
+      sx={{
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <FormGroup
         sx={{
-          justifyContent: "center",
+          mt: 10,
           alignItems: "center",
+          alignContent: "center",
         }}
       >
-        <FormGroup
+        <Box>
+          <img src={Logo} alt='Logi img'></img>
+        </Box>
+
+        <Typography variant='h4' color={"#ffff"}>
+          Restreador
+        </Typography>
+
+        <Input
+          className='inputLogin'
           sx={{
-            mt: 10,
-            alignItems: "center",
-            alignContent: "center",
+            px: 2,
+            m: 3,
+            maxWidth: 400,
+            border: 1,
+            borderRadius: 32,
+            borderColor: "#CF7A17",
+            color: "#ffff",
           }}
+          disableUnderline
+          size='md'
+          type='text'
+          name='textcode'
+          placeholder='Introduzca el Código del Producto'
+          id='textcode'
+          fullWidth
+        />
+
+        <Button
+          sx={{
+            maxWidth: 400,
+            backgroundColor: "#CF7A17",
+            borderRadius: 32,
+            color: "#fff",
+          }}
+          size='sm'
+          type='submit'
+          fullWidth
         >
-          <Typography sx={{ mt: 5 }} variant='h4' color={"#ffff"}>
-            Restreador
-          </Typography>
-
-          <Input
-            className='inputLogin'
-            sx={{
-              px: 2,
-              m: 3,
-              maxWidth: 400,
-              border: 1,
-              borderRadius: 32,
-              borderColor: "#CF7A17",
-              color: "#ffff",
-            }}
-            st
-            size='md'
-            type='email'
-            name='email'
-            placeholder='Email'
-            id='email'
-            fullWidth
-          />
-          <Input
-            sx={{
-              px: 2,
-              m: 3,
-              maxWidth: 400,
-              border: 1,
-              borderRadius: 32,
-              borderColor: "#CF7A17",
-              color: "#ffff",
-            }}
-            variant='plain'
-            size='md'
-            type='password'
-            name='password'
-            placeholder='Contraseña'
-            fullWidth
-          />
-
-          <Button
-            sx={{
-              maxWidth: 400,
-              backgroundColor: "#CF7A17",
-              borderRadius: 32,
-              color: "#fff",
-            }}
-            size='sm'
-            type='submit'
-            fullWidth
-          >
-            Iniciar Sesión
-          </Button>
-        </FormGroup>
-      </Box>
-    </Layout>
+          Verificar
+        </Button>
+      </FormGroup>
+    </Box>
   );
 };
